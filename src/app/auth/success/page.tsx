de5cb755b6dc411821f439951d4ef36f8e21b3b9
Michał Szwindowski <github@nedi.me>
2026-02-01 20:16:01 +0100

    #3.5

---
+++ src/app/auth/success/page.tsx
@@ -0,0 +1,16 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+export default function SuccessPage() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
+          <h1 className="text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+            Success
+          </h1>
+        </div>
+      </main>
+    </div>
+  );
+}
